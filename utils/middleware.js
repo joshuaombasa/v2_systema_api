b681ec233071430673376f428d6672fc4be8b33e
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-03-07 13:13:06 +0300

    initial commit

--- utils/middleware.js
+++ utils/middleware.js
@@ -2,7 +2,7 @@ const logger = require('./logger')
 
 const requestlogger = (request, response, next) => {
     logger.info('Method', request.method)
-    logger.info('Path', request.pat)
+    logger.info('Path', request.path)
     logger.info('Body', request.body)
     logger.info('__')
     next()
@@ -14,7 +14,12 @@ const unknownendpointHandler =  (request, response, next) => {
 
 const errorHandling = (error,request, response, next) => {
     logger.error(error.message)
-    next()
+    if (error.name === 'CastError') {
+        return response.status(400).json({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+    next(error)
 }
 
 module.exports = {
